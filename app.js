a224748d9edbb97039c8216793919fedf4a7d38a
Jiniyas Shah <72019447+jiniyasshah@users.noreply.github.com>
2024-01-24 21:34:01 +0545

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 async function generateUniqueCode() {
   // Generate a random 4-character string
@@ -31,7 +31,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 mongoose.connect(
-  "mongodb+srv://sapanats:Paypal123@cluster0.yc96cdz.mongodb.net/mapdb"
+  process.env.MONGODB_URI
 );
 
 const dataSchema = new mongoose.Schema({
